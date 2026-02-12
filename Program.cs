0fcb740a5d8e457ac334a4c2c4b0e56651ee0711
Sofus <sofusboelskifte91@gmail.com>
2026-02-12 18:09:20 +0100

    Stjerne program

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hvilket bilmærke? ");
+            string bilmærke = Console.ReadLine();
+            Console.WriteLine("Hvilken model? ");
+            string model = Console.ReadLine();
             Console.WriteLine("Hvilken brændstoftype kører bilen på? ");
             string brændstoftype = Console.ReadLine();
             Console.WriteLine("Hvor langt kører bilen per liter? ");
@@ -74,7 +78,24 @@
             Console.WriteLine("Tesla ".PadRight(15) + "| " + " Model Y ".PadRight(12) + "|" + " 100.000 km ".PadLeft(14));
             // Række 5
             Console.WriteLine("Audi ".PadRight(15) + "| " + " E-Tron ".PadRight(12) + "|" + " 75.000 km ".PadLeft(14));
+            // Række 6 input fra bruger
+            Console.WriteLine(bilmærke.PadRight(15) + "| " + " " + model.PadRight(11) + "|" + kilometerstand.ToString("N0", new System.Globalization.CultureInfo("da-DK")).PadLeft(10) + " km");
+            Console.WriteLine("-------------------------------------------------");
 
+            Console.WriteLine("\nTryk enter for at fortsætte");
+            Console.ReadLine();
+
+            // Tabel til hele bilen fra inputs
+            Console.WriteLine("Info ".PadRight(15) + "| " + "Værdier ".PadRight(12) + "|" + "Måleenheder ".PadLeft(14));
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("Bilmærke ".PadRight(15) + "| " + bilmærke.PadRight(12) + "|" + " ".PadLeft(14));
+            Console.WriteLine("Model ".PadRight(15) + "| " + model.PadRight(12) + "|" + " ".PadLeft(14));
+            Console.WriteLine("Kilometerstand ".PadRight(15) + "| " + kilometerstand.ToString("N0", new System.Globalization.CultureInfo("da-DK")).PadRight(12) + "|" + "km ".PadRight(14));
+            Console.WriteLine("Brændstoftype ".PadRight(15) + "| " + brændstoftype.PadRight(12) + "|" + " ".PadLeft(14));
+            Console.WriteLine("Kilometer pr/l ".PadRight(15) + "| " + kmperl.ToString().PadRight(12) + "|" + "km/l ".PadRight(14));
+            Console.WriteLine("Ny km stand ".PadRight(15) + "| " + Nykmantal.ToString("N0", new System.Globalization.CultureInfo("da-DK")).PadRight(12) + "|" + "km ".PadRight(14));
+            Console.WriteLine("Pris for turen ".PadRight(15) + "| " + pris.ToString().PadRight(12) + "|" + "kr ".PadRight(14));
+            Console.WriteLine("-----------------------------------------------------");
             Console.ReadLine(); //For en afslutning på denne del.
         }
     }
