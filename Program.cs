42c34c12902569cfedcb358c68a70b7a2295f9aa
Sofus <sofusboelskifte91@gmail.com>
2026-02-12 15:50:11 +0100

    IF ELSE ER MED

--- Program.cs
+++ Program.cs
@@ -7,9 +7,9 @@
             Console.WriteLine("Hvilken brændstoftype kører bilen på? ");
             string brændstoftype = Console.ReadLine();
             Console.WriteLine("Hvor langt kører bilen per liter? ");
-            int kmperl = Convert.ToInt32(Console.ReadLine());
+            double kmperl = double.Parse(Console.ReadLine());
             Console.WriteLine("Hvad er kilometerstanden på bilen? ");
-            string kilometerstand = Console.ReadLine();
+            int kilometerstand = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Brændstoftype " + brændstoftype);
             Console.WriteLine("KM per liter " + kmperl);
@@ -18,6 +18,40 @@
             Console.ReadLine();
 
             Console.WriteLine("Hvor langt skal du køre? ");
+            int afstand = int.Parse(Console.ReadLine());
+
+            double benzinPris = 13.49;
+            double dieselPris = 12.29;
+            double benzinForbrug = afstand / kmperl;
+            double fuelType = 0;
+
+            // IF ELSE METODE
+            if (brændstoftype.ToLower() == "benzin")
+            {
+                fuelType = benzinPris;
+                double pris = benzinForbrug * benzinPris;
+                Console.WriteLine("Din tur vil koste: " + pris);
+            }
+            else if (brændstoftype.ToLower() == "diesel")
+            {
+                fuelType = dieselPris;
+                double pris = benzinForbrug * dieselPris;
+                Console.WriteLine("Din tur vil koste: " + pris);
+            }
+            else
+            {
+                Console.WriteLine("Ukendt brændstoftype");
+                return;
+            }
+              
+            
+            
+            Console.ReadLine();
+
+            int Nykmantal = kilometerstand + afstand;
+            Console.WriteLine("Ny kilometer stand: " + Nykmantal);
+
+            Console.ReadLine();
 
         }
     }
