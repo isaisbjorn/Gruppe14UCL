973e08dc8556528df44757c5dbbe315d6a733951
Sofus <sofusboelskifte91@gmail.com>
2026-02-12 13:48:43 +0100

    test

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Gruppe14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hvilken brændstoftype kører bilen på? ");
+            string brændstoftype = Console.ReadLine();
+            Console.WriteLine("Hvor langt kører bilen per liter? ");
+            int kmperl = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hvad er kilometerstanden på bilen? ");
+            string kilometerstand = Console.ReadLine();
+
+            Console.WriteLine("Brændstoftype " + brændstoftype);
+            Console.WriteLine("KM per liter " + kmperl);
+            Console.WriteLine("Kilometerstand " + kilometerstand);
+
+            Console.ReadLine();
+
+            Console.WriteLine("Hvor langt skal du køre? ");
+
+        }
+    }
+}
