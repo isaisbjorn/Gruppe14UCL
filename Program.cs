20c914ad85f6a68aad10f79b9249205f6c280591
Noa <anlp79207@edu.ucl.dk>
2026-02-19 14:04:25 +0100

    Car Menu Integration

--- Program.cs
+++ Program.cs
@@ -1,130 +1,363 @@
-﻿namespace MinBilProjekt
+﻿using System.Runtime.InteropServices;
+
+namespace MinBilProjekt
 {
 	internal class Program
 	{
+		static string carType;
+		static string carModel;
+		static string carYear;
+		static string fuelType;
+		static double kmPerLiter;
+		static int carDistance;
+
+		static bool carCreated = false;
+		static bool isEngineOn = false;
+
 		static void Main(string[] args)
 		{
-			Console.WriteLine("===CARAPP===");
+			bool isRunning = true;
 
-			// Din bils oplysninger
-			Console.Write("Bil Type: ");
-			string carType = Console.ReadLine();
-
-			Console.Write("Bil Model: ");
-			string carModel = Console.ReadLine();
-
-			Console.Write("Bilens årgang: ");
-			string carYear = Console.ReadLine();
-
-			Console.Write("Brændstof Type: ");
-			string fuelType = Console.ReadLine();
-
-			Console.Write("KM per liter: ");
-			double kmPerLiter = double.Parse(Console.ReadLine()); // gem som double
-
-			Console.Write("Hvor langt har din bil kørt?: ");
-			int kmAfstand = int.Parse(Console.ReadLine());
-
-			// Output
-			Console.WriteLine();
-			Console.WriteLine("===Din bils oplysninger:====");
-			Console.WriteLine($"Din bil type: {carType}");
-            Console.WriteLine($"Din bil model: {carModel}");
-			Console.WriteLine($"Din bils brændstoftype er: {fuelType}");
-			Console.WriteLine($"Din bils KM/l er: {kmPerLiter}km");
-			Console.WriteLine($"Dil bil har kørt: {kmAfstand}km");
-
-
-			Console.WriteLine();
-
-			// Benzin/disel priser
-			double benzinPris = 13.49;
-			double dieselPris = 12.29;
-
-			// Ouput
-			Console.WriteLine($"Pris på benzin: {benzinPris}pr liter");
-			Console.WriteLine($"Pris på disel: {dieselPris}pr liter");
-
-			// Prisen på total kilometer kørt
-
-			double totalForbrug = kmAfstand / kmPerLiter;
-			double brændstofType = 0;
-
-
-			// Eneste måde, jeg lige kunne komme på en løsning (Conditional Statement)
-			if (fuelType.ToLower() == "benzin") // ToLower sikrer, at koden virker, hvis der er stavefejl
+			while (isRunning)
 			{
-				brændstofType = benzinPris; //benzinPris == 13.49
+				Console.Clear();
+				showMenu();
+				string input = Console.ReadKey(true).KeyChar.ToString();
+
+				switch (input)
+				{
+					case "1":
+						carDetails();
+						break;
+					case "2":
+						calculateFuelPricing();
+						break;
+					case "3":
+						updateDistance();
+						break;
+					case "4":
+						carInformation();
+						break;
+					case "5":
+						exitProgram();
+						break;
+					case "6":
+						Drive(10);
+						break;
+					case "7":
+						StartEngine();
+						break;
+					case "8":
+						StopEngine();
+						break;
+					case "9":
+						Console.Write("Enter a km value to test: ");
+						int testKm = int.Parse(Console.ReadLine());
+						bool result = IsPalindrome(testKm);
+						if (result)
+							Console.WriteLine($"{testKm} is a palindrome!");
+						else
+							Console.WriteLine($"{testKm} is NOT a palindrome!");
+
+						Console.WriteLine("\nPress any key to return...");
+						Console.ReadKey(true);
+						break;
+					default:
+						Console.WriteLine("Please type a valid option 1-8");
+						break;
+				}
 			}
-			else if (fuelType.ToLower() == "diesel") // ToLower sikrer, at koden virker, hvis der er stavefejl
+		}
+
+		static void showMenu()
+		{
+			Console.WriteLine("CarApp Menu");
+			Console.WriteLine("1) Enter your car's details");
+			Console.WriteLine("2) Calculate fuel pricing");
+			Console.WriteLine("3) Update your distance");
+			Console.WriteLine("4) Your cars information");
+			Console.WriteLine("5) Exit");
+			Console.WriteLine("6) Go for a Drive");
+			Console.WriteLine("7) Start Engine");
+			Console.WriteLine("8) Stop Engine");
+            Console.WriteLine("9) Check if Odometer is Palindrom");
+		}
+
+		static void carDetails()
+		{
+			Console.WriteLine("=== Enter Car Details ===\n");
+
+			Console.Write("Car Type: ");
+			carType = Console.ReadLine();
+
+			Console.Write("Car Model: ");
+			carModel = Console.ReadLine();
+
+			Console.Write("Car Year: ");
+			carYear = Console.ReadLine();
+
+			Console.Write("Fuel Type: ");
+			fuelType = Console.ReadLine();
+
+			Console.Write("KM per Liter: ");
+			kmPerLiter = double.Parse(Console.ReadLine());
+
+			Console.Write("Car Distance: ");
+			carDistance = int.Parse(Console.ReadLine());
+
+			carCreated = true;
+
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("\n=== Car Details Saved ===");
+			Console.ResetColor();
+
+			Console.WriteLine("\nPress any key to return to menu...");
+			Console.ReadKey(true);
+		}
+
+		static void calculateFuelPricing()
+		{
+			if (!carCreated)
 			{
-				brændstofType = dieselPris; // diselPris == 12.29
-			}
-			else
-			{
-				Console.WriteLine("Ukendt brændstoftype.");
+				Console.WriteLine("You must create a car first.");
+				Console.WriteLine("\nPress any key to return to menu...");
+				Console.ReadKey(true);
 				return;
 			}
 
-			double totalPris = totalForbrug * brændstofType;
+			Console.Write("Enter distance for the trip: ");
+			double distance = double.Parse(Console.ReadLine());
 
-			Console.WriteLine($"Den totale pris for {kmAfstand} km er: {totalPris:F0} kr");
+			double literPrice;
 
-			Console.WriteLine();
+			if (fuelType.ToLower() == "benzin")
+				literPrice = 13.49;
+			else if (fuelType.ToLower() == "diesel")
+				literPrice = 12.29;
+			else
+			{
+				Console.WriteLine("Invalid fuel type.");
+				Console.WriteLine("\nPress any key to return to menu...");
+				Console.ReadKey(true);
+				return;
+			}
 
-			// Opdateret Kilometerafstand
-			Console.Write("Indtast din nuværende Kilometer Afstand: ");
-			double gammelKMAfstand = double.Parse(Console.ReadLine());
+			double price = CalculateTripPrice(distance, literPrice, fuelType);
 
-			double nyKmAfstand = gammelKMAfstand + kmAfstand;
-			int afrundetAfstand = (int)Math.Round(nyKmAfstand);
+			Console.WriteLine($"\nTrip price: {price:F2} kr");
+			Console.WriteLine("\nPress any key to return to menu...");
+			Console.ReadKey(true);
+		}
 
-			Console.WriteLine(($"Din nye KM Afstand er: {afrundetAfstand}"));
+		static double CalculateTripPrice(double distance, double literPrice, string fuelType)
+		{
+			if (kmPerLiter == 0)
+			{
+				Console.WriteLine("KM per liter cannot be 0.");
+				return 0;
+			}
 
-			// Opdateret info Output
-			Console.WriteLine();
-			Console.WriteLine(" === OPDATERET INFO === ");
-			Console.WriteLine($"Bil Type: {carType}");
-			Console.WriteLine($"Bilens Model: {carModel}");
-			Console.WriteLine($"Bilens Årgang: {carYear}");
-			Console.WriteLine($"Brændstof type: {fuelType}");
-			Console.WriteLine($"Km/l: {kmPerLiter}");
-			Console.WriteLine($"Originale Afstand: {gammelKMAfstand}");
-			Console.WriteLine($"Nye Afstand {afrundetAfstand}");
-			Console.WriteLine($"Brændstof Udgift: {totalPris}");
-			Console.WriteLine();
+			if (fuelType.ToLower() != "benzin" && fuelType.ToLower() != "diesel")
+			{
+				Console.WriteLine("Invalid fuel type.");
+				return 0;
+			}
 
-			// String format
-			int carPrice = 20000;
-			int carInsurance = 5000;
-			Console.WriteLine(String.Format("Prisen på bilen er \n-{0} og forsikring koster \n-{1}", carPrice, carInsurance));
-            Console.WriteLine();
+			double literUsed = distance / kmPerLiter;
+			double totalPrice = literUsed * literPrice;
 
+			return totalPrice;
+		}
 
-			// Tabel over Bil Info
-			Console.WriteLine("TABEL MED DIN INFORMATION");
-			Console.WriteLine();
-			Console.WriteLine("==============================================================");
-			Console.WriteLine("| " + "Felt".PadRight(22) + "| " + "Værdi".PadRight(20) + "| " + "Enhed".PadRight(8) + "|");
-			Console.WriteLine("==============================================================");
-
-			// Kolonner
-			Console.WriteLine("| " + "Bil Type:".PadRight(22) + "| " + carType.PadRight(20) + "| " + "-".PadRight(8) + "|");
-			Console.WriteLine("| " + "Model:".PadRight(22) + "| " + carModel.PadRight(20) + "| " + "-".PadRight(8) + "|");
-			Console.WriteLine("| " + "Bilens årgang:".PadRight(22) + "| " + carYear.ToString().PadRight(20) + "| " + "-".PadRight(8) + "|");
-			Console.WriteLine("| " + "Brændstof type:".PadRight(22) + "| " + fuelType.PadRight(20) + "| " + "-".PadRight(8) + "|");
-			Console.WriteLine("| " + "Km/l:".PadRight(22) + "| " + kmPerLiter.ToString("F2").PadRight(20) + "| " + "km/l".PadRight(8) + "|");
-			Console.WriteLine("| " + "Original afstand:".PadRight(22) + "| " + gammelKMAfstand.ToString().PadRight(20) + "| " + "km".PadRight(8) + "|");
-			Console.WriteLine("| " + "Ny afstand:".PadRight(22) + "| " + afrundetAfstand.ToString().PadRight(20) + "| " + "km".PadRight(8) + "|");
-			Console.WriteLine("| " + "Brændstof udgift:".PadRight(22) + "| " + totalPris.ToString("F2").PadRight(20) + "| " + "kr".PadRight(8) + "|");
-			Console.WriteLine("==============================================================");
-			Console.WriteLine();
-
-
-
-			Console.ReadLine();
-
+		static void updateDistance()
+		{
 
 		}
+
+		static void carInformation()
+		{
+			if (!carCreated)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("Fill in info before you can see your details");
+				Console.ResetColor();
+				Console.WriteLine("\nPress any key to return to menu...");
+				Console.ReadKey(true);
+				return;
+			}
+
+			Console.ForegroundColor = ConsoleColor.Blue;
+			Console.WriteLine("Your car's information:\n");
+			Console.ResetColor();
+
+			Console.WriteLine($"CarType: {carType}");
+			Console.WriteLine($"Car Model: {carModel}");
+			Console.WriteLine($"Car Year: {carYear}");
+			Console.WriteLine($"Fuel Type: {fuelType}");
+			Console.WriteLine($"KM per Liter: {kmPerLiter}");
+			Console.WriteLine($"Car Distance: {carDistance}");
+
+			Console.WriteLine("\nPress any key to return to menu...");
+			Console.ReadKey(true);
+		}
+
+		static void exitProgram()
+		{
+			Environment.Exit(0);
+		}
+
+		static void Drive(double distance)
+		{
+			if (!isEngineOn)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("TURN ON THE FUCKING CAR.");
+				Console.ResetColor();
+				Console.WriteLine("\nPress any key to return to menu...");
+				Console.ReadKey(true);
+				return;
+			}
+
+			carDistance += (int)distance;
+
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine($"You drove {distance} km.");
+			Console.WriteLine($"New odometer reading: {carDistance} km.");
+			Console.ResetColor();
+
+			Console.WriteLine("\nPress any key to return to menu...");
+			Console.ReadKey(true);
+		}
+
+		static void StartEngine()
+		{
+			if (isEngineOn)
+				Console.WriteLine("Engine is already on.");
+			else
+			{
+				isEngineOn = true;
+				Console.WriteLine("Engine started.");
+			}
+
+			Console.WriteLine("\nPress any key to return to menu...");
+			Console.ReadKey(true);
+		}
+
+		static void StopEngine()
+		{
+			if (!isEngineOn)
+				Console.WriteLine("Engine is already off.");
+			else
+			{
+				isEngineOn = false;
+				Console.WriteLine("Engine stopped.");
+			}
+
+			Console.WriteLine("\nPress any key to return to menu...");
+			Console.ReadKey(true);
+		}
+
+		static bool IsPalindrome(int km)
+		{
+			string text = km.ToString();
+
+			for (int i = 0; i < text.Length / 2; i++)
+			{
+				if (text[i] != text[text.Length - 1 - i])
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+
+		/* === GAMMEL KODE TIL DATA ===
+        Console.WriteLine("===CARAPP===");
+
+        // Din bils oplysninger
+        Console.Write("Bil Type: ");
+        string carType = Console.ReadLine();
+
+        Console.Write("Bil Model: ");
+        string carModel = Console.ReadLine();
+
+        Console.Write("Bilens årgang: ");
+        string carYear = Console.ReadLine();
+
+        Console.Write("Brændstof Type: ");
+        string fuelType = Console.ReadLine();
+
+        Console.Write("KM per liter: ");
+        double kmPerLiter = double.Parse(Console.ReadLine());
+
+        Console.Write("Hvor langt har din bil kørt?: ");
+        int kmAfstand = int.Parse(Console.ReadLine());
+
+        // Output
+        Console.WriteLine();
+        Console.WriteLine("===Din bils oplysninger:====");
+        Console.WriteLine($"Din bil type: {carType}");
+        Console.WriteLine($"Din bil model: {carModel}");
+        Console.WriteLine($"Din bils brændstoftype er: {fuelType}");
+        Console.WriteLine($"Din bils KM/l er: {kmPerLiter}km");
+        Console.WriteLine($"Dil bil har kørt: {kmAfstand}km");
+
+        Console.WriteLine();
+
+        // Benzin/disel priser
+        double benzinPris = 13.49;
+        double dieselPris = 12.29;
+
+        Console.WriteLine($"Pris på benzin: {benzinPris}pr liter");
+        Console.WriteLine($"Pris på disel: {dieselPris}pr liter");
+
+        double totalForbrug = kmAfstand / kmPerLiter;
+        double brændstofType = 0;
+
+        if (fuelType.ToLower() == "benzin")
+        {
+            brændstofType = benzinPris;
+        }
+        else if (fuelType.ToLower() == "diesel")
+        {
+            brændstofType = dieselPris;
+        }
+        else
+        {
+            Console.WriteLine("Ukendt brændstoftype.");
+            return;
+        }
+
+        double totalPris = totalForbrug * brændstofType;
+
+        Console.WriteLine($"Den totale pris for {kmAfstand} km er: {totalPris:F0} kr");
+
+        Console.WriteLine();
+
+        Console.Write("Indtast din nuværende Kilometer Afstand: ");
+        double gammelKMAfstand = double.Parse(Console.ReadLine());
+
+        double nyKmAfstand = gammelKMAfstand + kmAfstand;
+        int afrundetAfstand = (int)Math.Round(nyKmAfstand);
+
+        Console.WriteLine($"Din nye KM Afstand er: {afrundetAfstand}");
+
+        Console.WriteLine();
+        Console.WriteLine(" === OPDATERET INFO === ");
+        Console.WriteLine($"Bil Type: {carType}");
+        Console.WriteLine($"Bilens Model: {carModel}");
+        Console.WriteLine($"Bilens Årgang: {carYear}");
+        Console.WriteLine($"Brændstof type: {fuelType}");
+        Console.WriteLine($"Km/l: {kmPerLiter}");
+        Console.WriteLine($"Originale Afstand: {gammelKMAfstand}");
+        Console.WriteLine($"Nye Afstand {afrundetAfstand}");
+        Console.WriteLine($"Brændstof Udgift: {totalPris}");
+
+        int carPrice = 20000;
+        int carInsurance = 5000;
+        Console.WriteLine(String.Format("Prisen på bilen er \n-{0} og forsikring koster \n-{1}", carPrice, carInsurance));
+        */
 	}
 }
