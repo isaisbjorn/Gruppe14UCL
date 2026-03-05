279225e35d034aae5575ed614a629af8824767f1
Sofus <sofusboelskifte91@gmail.com>
2026-03-05 13:09:15 +0100

    Rodebutik

--- Car.cs
+++ Car.cs
@@ -5,102 +5,133 @@ using System.Transactions;
 
 namespace Gruppe14
 {
-    
-        public class Car
+    public class Car
+    {
+        // Attributter (private variabler der gemmer data om bilen)
+        private string brand;
+        private string model;
+        private int year;
+        private char gear;
+        private double odometer;
+        private bool isEngineOn;
+        private double kmPerLiter;
+
+        // Liste der gemmer alle ture bilen har kørt
+        private List<Trip> _trips = new List<Trip>();
+
+
+        // Konstruktør
+        // Bruges når man opretter en ny bil
+        public Car(string brand, string model, int year, char gear, FuelType fuelType, double kmPerLiter)
         {
-            // Attributter og indkapsling
-            private string brand;
-            private string model;
-            private int year;
-            private char gear;
-            private double odometer;
-            private bool isEngineOn;
-            private double kmPerLiter;
-            //Liste der gemmer alle ture bilen har kørt
-            private List<Trip> _trips = new List<Trip>();
+            // Gemmer værdierne i klassens variabler
+            this.brand = brand;
+            this.model = model;
+            this.year = year;
+            this.gear = gear;
 
-            // Konstruktør
-            public Car(string brand, string model, int year, char gear, FuelType fuelType, double kmPerLiter)
+            FuelType = fuelType;
+            this.kmPerLiter = kmPerLiter;
 
+            // Startværdier når bilen oprettes
+            this.odometer = 0;
+            this.isEngineOn = false;
+        }
+
+
+        // Properties
+        // Giver adgang til værdierne uden at andre klasser kan ændre dem direkte
+        public string Brand => brand;
+        public string Model => model;
+        public int Year => year;
+
+        // Gear kan både læses og ændres
+        public char Gear
+        {
+            get { return gear; }
+            set { gear = value; }
+        }
+
+        // Odometer viser hvor langt bilen har kørt
+        public double Odometer => odometer;
+
+        // Hvor langt bilen kører per liter
+        public double KmPerLiter => kmPerLiter;
+
+        // Viser om motoren er tændt
+        public bool IsEngineOn => isEngineOn;
+
+        // Enum der viser hvilken brændstoftype bilen bruger
+        public FuelType FuelType { get; private set; }
+
+
+        // Returnerer listen med alle ture
+        public List<Trip> GetTrips()
+        {
+            return _trips;
+        }
+
+
+        // Metode der tænder eller slukker motoren
+        public void ToggleEngine()
+        {
+            // Skifter værdien fra true til false eller omvendt
+            isEngineOn = !isEngineOn;
+        }
+
+
+        // Metode til at køre bilen en distance
+        public void Drive(double distance)
+        {
+            // Bilen kan kun køre hvis motoren er tændt og distancen er positiv
+            if (isEngineOn && distance > 0)
             {
-                this.brand = brand;
-                this.model = model;
-                this.year = year;
-                this.gear = gear;
-                this.kmPerLiter = kmPerLiter;
-                FuelType = fuelType;
-                this.kmPerLiter = kmPerLiter;
-                // Startværdier
-                this.odometer = 0;
-                this.isEngineOn = false;
+                // Lægger distance til kilometerstanden
+                odometer += distance;
             }
+        }
 
-            // Properties (get / set)
-            public string Brand => brand;
-            public string Model => model;
-            public int Year => year;
 
-            public char Gear
+        // Beregner hvad en tur koster i brændstof
+        public double CalculateTripPrice(double distance, double literPrice)
+        {
+            // Hvis værdierne er ugyldige returneres 0
+            if (distance <= 0 || literPrice <= 0)
+                return 0;
+
+            // Beregner hvor meget brændstof der bruges
+            double fueldUsed = distance / kmPerLiter;
+
+            // Returnerer prisen for turen
+            return fueldUsed * literPrice;
+        }
+
+
+        // Returnerer en tekst med oplysninger om bilen
+        public string GetCarDetails()
+        {
+            return $"{brand} {model} ({year}) - {FuelType} - {odometer} km - Motor: {(isEngineOn ? "Tændt" : "Slukket")}";
+        }
+
+
+        // Metode der tilføjer en tur til bilen
+        public void Drive(Trip newTrip)
+        {
+            // Tjekker at turen tilhører denne bil
+            if (newTrip.Car == this)
             {
-                get { return gear; }
-                set { gear = value; }
-            }
-
-            public double Odometer => odometer;
-            public double KmPerLiter => kmPerLiter;
-            public bool IsEngineOn => isEngineOn;
-
-            public FuelType FuelType { get; private set; }
-            public List<Trip> GetTrips()
-            {
-                return _trips;
-            }
-
-            //Metode: Tænd/sluk motor
-            public void ToggleEngine()
-            {
-                isEngineOn = !isEngineOn;
-            }
-
-            // Kør bilen
-            public void Drive(double distance)
-            {
-                 if (isEngineOn && distance > 0)
-                    {
-                        odometer += (int)distance;
-                    }
-            }
-            
-            // Beregn turprisen
-            public double CalculateTripPrice(double distance, double literPrice) 
-            {
-                if (distance <= 0 || literPrice <= 0)
-                    return 0;
-
-                double fueldUsed = distance / kmPerLiter;
-                return fueldUsed * literPrice; 
-
-            }
-            
-            public string GetCarDetails()
-            {
-                return $"{brand} {model} ({year}) - {FuelType} - {odometer} km - Motor: {(isEngineOn ? "Tændt" : "Slukket")}";
-            }
-            
-            public void Drive(Trip newTrip)
-            {
-                if (newTrip.Car == this)
-                {
+                // Opdaterer kilometerstanden
                 odometer += newTrip.Distance;
 
+                // Gemmer turen i listen
                 _trips.Add(newTrip);
-                }
-                else
-                {
+            }
+            else
+            {
+                // Hvis turen ikke tilhører bilen vises en fejl
                 Console.WriteLine("Fejl: Denne tur tilhører ikke denne bil.");
-                }
             }
         }
     }
-
+}
 
--- Program.cs
+++ Program.cs
@@ -6,136 +6,178 @@ namespace Gruppe14
     {
         static void Main(string[] args)
         {
+            // Opretter to Car objekter med forskellige oplysninger
             Car myCar1 = new Car("Toyota", "Corolla", 2020, 'A', FuelType.Benzin, 22.5);
             Car myCar2 = new Car("Nissan", "Qashqai", 2017, 'M', FuelType.Diesel, 17.8);
 
+            // Udskriver information om bilerne
             Console.WriteLine(myCar1.GetCarDetails());
             Console.WriteLine(myCar2.GetCarDetails());
 
+            // Starter motoren på bilen
             myCar1.ToggleEngine();
+
+            // Kører en tur på 120 km
             myCar1.Drive(120);
+
+            // Udskriver nye oplysninger efter kørslen
             Console.WriteLine("\n Efter Kørsel:");
             Console.WriteLine(myCar1.GetCarDetails());
 
+            // Beregner hvad turen kostede i brændstof
             double tripPrice = myCar1.CalculateTripPrice(120, 14.5);
             Console.WriteLine($"Turen kostede: {tripPrice} kr.");
 
+            // Stopper programmet indtil brugeren trykker enter
             Console.ReadLine();
 
+
+            // Opretter endnu en bil
             Car myCar3 = new Car("Toyota", "Corolla", 2020, 'A', FuelType.Benzin, 22.5);
+
+            // Starter motoren
             myCar3.ToggleEngine();
 
+
+            // Opretter en liste med ture
             List<Trip> trips = new List<Trip>
-            {
-                new Trip(myCar3, 50, DateTime.Now, DateTime.Now.AddHours(1)),
-                new Trip(myCar3, 30, DateTime.Now, DateTime.Now.AddMinutes(30)),
-                new Trip(myCar3, 100, DateTime.Now, DateTime.Now.AddHours(2))
-            };
-            
+        {
+            new Trip(myCar3, 50, DateTime.Now, DateTime.Now.AddHours(1)),
+            new Trip(myCar3, 30, DateTime.Now, DateTime.Now.AddMinutes(30)),
+            new Trip(myCar3, 100, DateTime.Now, DateTime.Now.AddHours(2))
+        };
+
+            // Gennemgår listen og sender hver tur til bilen
             foreach (var trip in trips)
             {
                 myCar3.Drive(trip);
             }
+
+            // Udskriver alle ture som bilen har kørt
             Console.WriteLine("\n Alle ture for bilen");
             foreach (var trip in myCar3.GetTrips())
             {
                 Console.WriteLine(trip.GetTripDetails());
             }
-            
 
+
+            // Spørger brugeren om oplysninger om en bil
             Console.WriteLine("Hvilket bilmærke? ");
             string bilmærke = Console.ReadLine();
+
             Console.WriteLine("Hvilken model? ");
             string model = Console.ReadLine();
+
             Console.WriteLine("Hvilken brændstoftype kører bilen på? ");
             string brændstoftype = Console.ReadLine();
+
             Console.WriteLine("Hvor langt kører bilen per liter? ");
             double kmperl = double.Parse(Console.ReadLine());
+
             Console.WriteLine("Hvad er kilometerstanden på bilen? ");
             int kilometerstand = int.Parse(Console.ReadLine());
 
+            // Udskriver de værdier brugeren har indtastet
             Console.WriteLine("Brændstoftype " + brændstoftype);
             Console.WriteLine("KM per liter " + kmperl + "Km");
             Console.WriteLine("Kilometerstand " + kilometerstand + "Km");
 
 
+            // Spørger hvor langt brugeren vil køre
             Console.WriteLine("Hvor langt skal du køre? ");
             int afstand = int.Parse(Console.ReadLine());
 
+            // Brændstofpriser
             double benzinPris = 13.49;
             double dieselPris = 12.29;
+
+            // Beregner hvor meget brændstof turen bruger
             double benzinForbrug = afstand / kmperl;
+
             double fuelType = 0;
             double pris = 0;
 
-            // IF ELSE METODE
-            // Definer double, int osv uden for if else, så det stadig kan bruges i resten af koden.
+            // IF/ELSE der afgør hvilken pris der skal bruges
             if (brændstoftype.ToLower() == "benzin")
             {
                 fuelType = benzinPris;
                 pris = benzinForbrug * benzinPris;
+
                 Console.WriteLine("Din tur vil koste: {0} kr", pris);
             }
             else if (brændstoftype.ToLower() == "diesel")
             {
                 fuelType = dieselPris;
                 pris = benzinForbrug * dieselPris;
-                Console.WriteLine(
-                    string.Format("Din tur vil koste: {0} kr", pris));
+
+                Console.WriteLine(string.Format("Din tur vil koste: {0} kr", pris));
             }
             else
             {
+                // Hvis brugeren skriver noget andet end benzin eller diesel
                 Console.WriteLine("Ukendt brændstoftype");
                 return;
             }
-                        
+
             Console.ReadLine();
 
+            // Beregner ny kilometerstand efter turen
             int Nykmantal = kilometerstand + afstand;
-            
+
             Console.ReadLine();
 
+            // Udskriver en oversigt over bilens oplysninger
             Console.WriteLine("====BILENS OPLYSNINGER====");
             Console.WriteLine("Brændstoftype: " + brændstoftype);
             Console.WriteLine("Kilometer per liter: " + kmperl + "Km");
             Console.WriteLine("Oprindelig kilometerstand " + kilometerstand + "Km");
             Console.WriteLine("Ny kilometerstand " + Nykmantal + "Km");
             Console.WriteLine("Brændstofudgift " + pris + "kr");
-            Console.ReadLine();
-            
 
-            // Overskrift
+            Console.ReadLine();
+
+
+            // Laver en tabel med forskellige biler
             Console.WriteLine("Bilmærke ".PadRight(15) + "| " + " Model ".PadRight(12) + "|" + " Kilometertal ".PadLeft(14));
             Console.WriteLine("-------------------------------------------------");
-            // Række 1
+
             Console.WriteLine("Toyota ".PadRight(15) + "| " + " Corolla ".PadRight(12) + "|" + " 156.000 km ".PadLeft(14));
-            // Række 2
             Console.WriteLine("Ford ".PadRight(15) + "| " + " Fiesa ".PadRight(12) + "|" + " 112.000 km ".PadLeft(14));
-            // Række 3
             Console.WriteLine("Skoda ".PadRight(15) + "| " + " Citigo ".PadRight(12) + "|" + " 225.000 km ".PadLeft(14));
-            // Række 4
             Console.WriteLine("Tesla ".PadRight(15) + "| " + " Model Y ".PadRight(12) + "|" + " 100.000 km ".PadLeft(14));
-            // Række 5
             Console.WriteLine("Audi ".PadRight(15) + "| " + " E-Tron ".PadRight(12) + "|" + " 75.000 km ".PadLeft(14));
-            // Række 6 input fra bruger
-            Console.WriteLine(bilmærke.PadRight(15) + "| " + " " + model.PadRight(11) + "|" + kilometerstand.ToString("N0", new System.Globalization.CultureInfo("da-DK")).PadLeft(10) + " km");
+
+            // Udskriver også bilen som brugeren har indtastet
+            Console.WriteLine(bilmærke.PadRight(15) + "| " + " " + model.PadRight(11) + "|" +
+                kilometerstand.ToString("N0", new System.Globalization.CultureInfo("da-DK")).PadLeft(10) + " km");
+
             Console.WriteLine("-------------------------------------------------");
 
             Console.WriteLine("\nTryk enter for at fortsætte");
             Console.ReadLine();
 
-            // Tabel til hele bilen fra inputs
+
+            // Laver en tabel med alle oplysninger om bilen
             Console.WriteLine("Info ".PadRight(15) + "| " + "Værdier ".PadRight(12) + "|" + "Måleenheder ".PadLeft(14));
             Console.WriteLine("-----------------------------------------------------");
+
             Console.WriteLine("Bilmærke ".PadRight(15) + "| " + bilmærke.PadRight(12) + "|" + " ".PadLeft(14));
             Console.WriteLine("Model ".PadRight(15) + "| " + model.PadRight(12) + "|" + " ".PadLeft(14));
-            Console.WriteLine("Kilometerstand ".PadRight(15) + "| " + kilometerstand.ToString("N0", new System.Globalization.CultureInfo("da-DK")).PadRight(12) + "|" + "km ".PadRight(14));
+            Console.WriteLine("Kilometerstand ".PadRight(15) + "| " +
+                kilometerstand.ToString("N0", new System.Globalization.CultureInfo("da-DK")).PadRight(12) + "|" + "km ".PadRight(14));
+
             Console.WriteLine("Brændstoftype ".PadRight(15) + "| " + brændstoftype.PadRight(12) + "|" + " ".PadLeft(14));
             Console.WriteLine("Kilometer pr/l ".PadRight(15) + "| " + kmperl.ToString().PadRight(12) + "|" + "km/l ".PadRight(14));
-            Console.WriteLine("Ny km stand ".PadRight(15) + "| " + Nykmantal.ToString("N0", new System.Globalization.CultureInfo("da-DK")).PadRight(12) + "|" + "km ".PadRight(14));
+
+            Console.WriteLine("Ny km stand ".PadRight(15) + "| " +
+                Nykmantal.ToString("N0", new System.Globalization.CultureInfo("da-DK")).PadRight(12) + "|" + "km ".PadRight(14));
+
             Console.WriteLine("Pris for turen ".PadRight(15) + "| " + pris.ToString().PadRight(12) + "|" + "kr ".PadRight(14));
+
             Console.WriteLine("-----------------------------------------------------");
-            Console.ReadLine(); //For en afslutning på denne del.
+
+            // Stopper programmet før det lukker
+            Console.ReadLine();
         }
     }
 }
